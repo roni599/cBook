15b1d2c1328ae0ce0f5db932d7440d44b2867af2
Md Nezam Uddin <112230389+nezamcst@users.noreply.github.com>
2024-10-23 13:25:37 +0600

    favicon icon change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>cBook</title>
-    <link rel="icon" href="{{ asset('backend/assets/img/cBook1.jpeg') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('backend/assets/img/cBook3.jpeg') }}" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{ asset('backend') }}/css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
